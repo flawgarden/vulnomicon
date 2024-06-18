8d33b14877fa1f2de0518708d1a010c4c19a0ceb
Aleksei Babushkin <ocelaiwo@gmail.com>
2024-06-18 21:28:31 +0300

    ci: Adapt to new bentoo release system

--- scripts/bentoo.sh
+++ scripts/bentoo.sh
@@ -5,11 +5,11 @@ BASE_DIR="$(cd "$SCRIPT_DIR"/.. && pwd)"
 
 cd "$BASE_DIR" || exit;
 
-wget https://github.com/flawgarden/bentoo/releases/download/latest/bentoo.tar.gz
+wget https://github.com/flawgarden/bentoo/releases/download/latest/bentoo-x86-linux.tar.gz
 wget https://github.com/flawgarden/bentoo/releases/download/latest/tool_runners.tar.gz
 
-tar -xf bentoo.tar.gz
+tar -xf bentoo-x86-linux.tar.gz
 tar -xf tool_runners.tar.gz
 
-rm bentoo.tar.gz tool_runners.tar.gz
+rm bentoo-x86-linux.tar.gz tool_runners.tar.gz
 
