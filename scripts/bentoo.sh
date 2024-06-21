b74443c07c73da796f7f8ee616b946a76551526a
Aleksei Babushkin <ocelaiwo@gmail.com>
2024-06-21 19:41:01 +0500

    feat: Download bentoo binary according to OS and CPU archtecture

--- scripts/bentoo.sh
+++ scripts/bentoo.sh
@@ -2,14 +2,39 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BASE_DIR="$(cd "$SCRIPT_DIR"/.. && pwd)"
+ARCH=$(uname -m)
 
 cd "$BASE_DIR" || exit;
 
-wget https://github.com/flawgarden/bentoo/releases/download/latest/bentoo-x86-linux.tar.gz
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        echo "Downloading bentoo binary for Mac OS / x86_64"
+        ARCHIVE_NAME=bentoo-x86-macos.tar.gz
+    elif [[ "$ARCH" == "arm64" ]]; then
+        echo "Downloading bentoo binary for Mac OS / ARM64"
+        ARCHIVE_NAME=bentoo-arm-macos.tar.gz
+    else
+        echo "OS/CPU Architecture unsupported"
+        exit 1
+    fi
+elif [[ "$OSTYPE" == "linux"* ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        echo "Downloading bentoo binary for Linux / x86_64"
+        ARCHIVE_NAME=bentoo-x86-linux.tar.gz
+    else
+        echo "OS/CPU Architecture unsupported"
+        exit 1
+    fi
+else
+    echo "OS/CPU Architecture unsupported"
+    exit 1
+fi
+
+wget https://github.com/flawgarden/bentoo/releases/download/latest/$ARCHIVE_NAME
+tar -xf $ARCHIVE_NAME
+rm $ARCHIVE_NAME
+
+# Download tool runners
 wget https://github.com/flawgarden/bentoo/releases/download/latest/tool_runners.tar.gz
-
-tar -xf bentoo-x86-linux.tar.gz
 tar -xf tool_runners.tar.gz
-
-rm bentoo-x86-linux.tar.gz tool_runners.tar.gz
-
+rm tool_runners.tar.gz
