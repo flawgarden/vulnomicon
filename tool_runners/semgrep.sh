2845e9e301c4087ddd4a3057a0cec46dd9ca2f44
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-12-24 05:51:58 +0300

    fix: Pin `semgrep` version: new versions missing the `rules` field due to the new business policy (#50)

--- tool_runners/semgrep.sh
+++ tool_runners/semgrep.sh
@@ -3,17 +3,17 @@ set -e
 
 # Check for install/updates at https://github.com/returntocorp/semgrep
 
-docker pull semgrep/semgrep >&2
+docker pull semgrep/semgrep:1.99.0 >&2
 
 entry_point=$1
-cd $entry_point
+cd "$entry_point"
 
-semgrep_version=$(docker run --rm semgrep/semgrep semgrep --version)
+semgrep_version=$(docker run --rm semgrep/semgrep:1.99.0 semgrep --version)
 result_filename="Semgrep-v$semgrep_version.sarif"
 
-docker run --rm -v "${PWD}:/src" semgrep/semgrep semgrep --config auto -q --sarif -o "$result_filename" . >&2
-docker run --rm -v "${PWD}:/src" ubuntu sh -c "chown $(id -u $USER):$(id -g $USER) -R /src" >&2
+docker run --rm -v "${PWD}:/src" semgrep/semgrep:1.99.0 semgrep --config auto -q --sarif -o "$result_filename" . >&2
+docker run --rm -v "${PWD}:/src" ubuntu sh -c "chown $(id -u "$USER"):$(id -g "$USER") -R /src" >&2
 
 result_file="$entry_point/$result_filename"
 
-cat $result_file
+cat "$result_file"
